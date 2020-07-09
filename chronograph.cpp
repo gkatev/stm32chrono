24a729e668c502349d47156d908c08cb025da7a1
gkatev <george_kate@hotmail.com>
2020-07-10 01:18:32 +0300

    Added some more code, including for timers. Added options in header file.

--- chronograph.cpp
+++ chronograph.cpp
@@ -1,11 +1,215 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 
 #include <core/usb_vcp.h>
 #include <core/millis.h>
 
+#include "chronograph.h"
 #include "peak.h"
+
+// --------------------------------------------
+
+volatile enum {front_s, back_s, timeout_s} state;
+
+// --------------------------------------------
+
+void adc_init();
+void adc_channel(uint8_t channel);
+uint32_t adc_read();
+void gpio_adc_init();
+
+void timer_init();
+uint16_t timer_read();
+void timer_start();
+void timer_stop();
+
+void chrono();
+float calc_fps(uint16_t ticks);
+
+// --------------------------------------------
+
+extern "C" void tim2_isr(void) {
+	if(timer_get_flag(TIM2, TIM_SR_UIF)) {
+		timer_clear_flag(TIM2, TIM_SR_UIF);
+		
+		state = timeout_s;
+	}
+}
+
+int main() {
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	
+	millis_init();
+	vcp_init();
+	
+	gpio_adc_init();
+	adc_init();
+	
+	chrono();
+	
+	// extern void test();
+	// test();
+}
+
+void chrono() {
+	struct peak_stat pstat;
+	uint16_t samples[PEAK_LAG];
+	
+	peak_stat_init(pstat, PEAK_THRESHOLD,
+		PEAK_INFLUENCE, PEAK_LAG, samples);
+	
+	adc_channel(CHANNEL_FRONT);
+	state = front_s;
+	
+	vcp_printf("Measuring!\n");
+	
+	while(1) {
+		uint16_t timer_val, adc_val;
+		
+		if(state == timeout_s) {
+			adc_channel(CHANNEL_FRONT);
+			state = front_s;
+			
+			#ifdef DEBUG
+				vcp_printf("TIMEOUT\n");
+			#endif
+		}
+		
+		timer_val = timer_read();
+		adc_val = adc_read();
+		
+		if(!peak_detect(pstat, adc_val))
+			continue;
+		
+		/* Peak detected */
+		
+		if(state == front_s) {
+			timer_start();
+			
+			adc_channel(CHANNEL_REAR);
+			state = back_s;
+			
+			#ifdef DEBUG
+				vcp_printf("FD\n");
+			#endif
+		} else if(state == back_s) {
+			timer_stop();
+			
+			float fps = calc_fps(timer_val);
+			vcp_printf("FPS = %f\n", fps);
+		}
+		
+		peak_stat_reset(pstat);
+	}
+}
+
+float calc_fps(uint16_t ticks) {
+	if(ticks == 0) {
+		#ifdef DEBUG
+			vcp_printf("ticks: 0\n");
+		#endif
+		
+		return 0;
+	}
+	
+	float dt_us = TICKS_TO_US(ticks);
+	float speed = DISTANCE_UM / dt_us * SPEED_CALIBRATION_FACTOR;
+	float fps = speed * MPS_TO_FPS_FACTOR;
+	
+	#ifdef DEBUG
+		vcp_printf("ticks: %u dt(us): %u U(m/s): %f U(fps): %f\n",
+			ticks, dt_us, speed, fps);
+	#endif
+	
+	return fps;
+}
+
+void test() {
+	/* while(1) {
+		uint32_t t = millis();
+		
+		for(int i = 0; i < 1000000; i++) {
+			uint32_t val = adc_read();
+		}
+		
+		uint32_t t2 = millis();
+		
+		vcp_printf("time = %u ms\n", t2 - t);
+	} */
+	
+	/* uint32_t last_adc = 0xFFFFFFFF;
+	
+	while(1) {
+		uint32_t adc_val = adc_read();
+		
+		if(adc_val != last_adc) {
+			vcp_printf("ADC: %u\n", adc_val);
+			last_adc = adc_val;
+		}
+	} */
+	
+	/* while(1) {
+		uint16_t adc_val = adc_read();
+		
+		// vcp_printf("ADC: %d\n", adc_val);
+		
+		if(peak_detect(pstat, adc_val)) {
+			uint16_t average = pstat.sample_sum / pstat.elements;
+			vcp_printf("Peak: %u, Average: %u, Diff: %u\n",
+				adc_val, average, adc_val - average);
+			
+			peak_stat_reset(pstat);
+		}
+	} */
+	
+	/* while(1) {
+		uint32_t t = millis();
+		
+		for(int i = 0; i < 1000000; i++) {
+			uint16_t adc_val = adc_read();
+			peak_detect(pstat, adc_val);
+			
+			// if(adc_val > 10000000)
+				// vcp_printf("FAKE");
+		}
+		
+		uint32_t t2 = millis();
+		
+		vcp_printf("time = %u ms\n", t2 - t);
+	} */
+	
+	/* timer_init();
+	
+	vcp_printf("starting\n");
+	
+	while(true) {
+		state = front_s;
+		
+		timer_enable_counter(TIM2);
+		uint32_t t = millis();
+		
+		// while(state != timeout_s) {}
+		
+		while(state != timeout_s) {
+			if(millis() - t > 200) {
+				timer_stop();
+				delay_ms(200);
+				timer_start();
+				
+				while(state != timeout_s);
+			}
+		}
+		
+		uint32_t t2 = millis();
+		
+		vcp_printf("time = %u ms\n", t2 - t);
+	} */
+}
+
+// --------------------------------------------
 
 void adc_init() {
 	rcc_periph_clock_enable(RCC_ADC1);
@@ -40,79 +244,37 @@ uint32_t adc_read() {
 void gpio_adc_init() {
     rcc_periph_clock_enable(RCC_GPIOA);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
+    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO1);
 }
 
-int main() {
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+void timer_init() {
+	rcc_periph_clock_enable(RCC_TIM2);
+	rcc_periph_reset_pulse(RST_TIM2);
 	
-	millis_init();
-	vcp_init();
+	timer_set_prescaler(TIM2, (rcc_apb1_frequency * 2) / TIMER_FREQ);
+	timer_set_period(TIM2, TIMER_ARR);
+	timer_one_shot_mode(TIM2);
 	
-	gpio_adc_init();
+	/* For some reason, the first time the counter is enabled,
+	 * UEV is generated immediately. So, we handle this here. */
+	timer_set_counter(TIM2, TIMER_ARR);
+	timer_enable_counter(TIM2);
+	while(!timer_get_flag(TIM2, TIM_SR_UIF));
+	timer_clear_flag(TIM2, TIM_SR_UIF);
 	
-	adc_init();
-	adc_channel(ADC_CHANNEL0);
-	
-	vcp_printf("Chronograph starting!\n");
-	
-	/* while(1) {
-		uint32_t t = millis();
-		
-		for(int i = 0; i < 1000000; i++) {
-			uint32_t val = adc_read();
-		}
-		
-		uint32_t t2 = millis();
-		
-		vcp_printf("time = %u ms\n", t2 - t);
-	} */
-	
-	/* uint32_t last_adc = 0xFFFFFFFF;
-	
-	while(1) {
-		uint32_t adc_val = adc_read();
-		
-		if(adc_val != last_adc) {
-			vcp_printf("ADC: %u\n", adc_val);
-			last_adc = adc_val;
-		}
-	} */
-	
-	#define PEAK_LAG 64
-	#define PEAK_THRESHOLD 80
-	
-	struct peak_stat pstat;
-	uint16_t samples[PEAK_LAG];
-	
-	peak_stat_init(pstat, PEAK_THRESHOLD, 0, PEAK_LAG, samples);
-	
-	while(1) {
-		uint16_t adc_val = adc_read();
-		
-		// vcp_printf("ADC: %d\n", adc_val);
-		
-		if(peak_detect(pstat, adc_val)) {
-			uint16_t average = pstat.sample_sum / pstat.elements;
-			vcp_printf("Peak: %u, Average: %u, Diff: %u\n",
-				adc_val, average, adc_val - average);
-			
-			peak_stat_reset(pstat);
-		}
-	}
-	
-	/* while(1) {
-		uint32_t t = millis();
-		
-		for(int i = 0; i < 1000000; i++) {
-			uint16_t adc_val = adc_read();
-			peak_detect(pstat, adc_val);
-			
-			// if(adc_val > 10000000)
-				// vcp_printf("FAKE");
-		}
-		
-		uint32_t t2 = millis();
-		
-		vcp_printf("time = %u ms\n", t2 - t);
-	} */
+	nvic_enable_irq(NVIC_TIM2_IRQ);
+	timer_enable_irq(TIM2, TIM_DIER_UIE);
+}
+
+uint16_t timer_read() {
+	return timer_get_counter(TIM2);
+}
+
+void timer_start() {
+	timer_set_counter(TIM2, 0);
+	timer_enable_counter(TIM2);
+}
+
+void timer_stop() {
+	timer_disable_counter(TIM2);
 }
---
+++ chronograph.h
@@ -0,0 +1,36 @@
+#ifndef CHRONOGRAPH_H
+#define CHRONOGRAPH_H
+
+#include <libopencm3/stm32/adc.h>
+
+// Verbose Output
+#define DEBUG
+
+// Photodiode ADC channels
+#define CHANNEL_FRONT ADC_CHANNEL0
+#define CHANNEL_REAR ADC_CHANNEL1
+
+// Peak Detection
+#define PEAK_LAG 64
+#define PEAK_THRESHOLD 80
+#define PEAK_INFLUENCE 0
+
+// Max ticks measured = TIMER_ARR
+// Max time measured = TIMER_ARR/TIMER_FREQ
+// One tick equals 1/TIMER_FREQ seconds
+#define TIMER_ARR 0xFFFF
+#define TIMER_FREQ ((int) 30e06)
+
+// Convert timer ticks to micro seconds
+#define TICKS_TO_US(ticks) ((float) (ticks) / TIMER_FREQ / 1e06)
+
+// Distance of diodes (10^-6 m)
+#define DISTANCE_UM 30000
+
+// Fine-grain calibration
+#define SPEED_CALIBRATION_FACTOR 1
+
+// Used to convert m/s to fps
+#define MPS_TO_FPS_FACTOR ((float) 3.2808398950131);
+
+#endif
